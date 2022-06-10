64630b2e9457f792ce956dea91ecfcaee8f75f83
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-10 10:02:43 +0200

    Add colors to Player class

--- mastermind.rb
+++ mastermind.rb
@@ -5,21 +5,17 @@ class Player
     @losses = 0
     @maker_guesses = 0
     @breaker_guesses = 0
+    @available_colors = %w[red green yellow orange blue purple]
   end
 end
 
 class Computer < Player
   def initialize
-    name = ["Saul Bitman", "Ternary Tristan", "Octal John","Marcus Loopius"].sample
+    name = ['Saul Bitman', 'Ternary Tristan', 'Octal John', 'Marcus Loopius'].sample
     super(name)
   end
 end
 
-
-
 cpu = Computer.new
 
 p cpu
-
-
-
